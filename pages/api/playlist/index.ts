b5bb1f15c1cc6447dd2e6176be663b5080dc9864
Daniel Ye <daniel.ye@live.com>
2023-01-02 17:56:46 -0500

    improved fuzzy matching for song names

--- pages/api/playlist/index.ts
+++ pages/api/playlist/index.ts
@@ -1,4 +1,5 @@
 const BASE_URI = `https://api.spotify.com/v1`;
+import fuzzysort from "fuzzysort";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -21,12 +22,13 @@ export default async function handler(req, res) {
       song: string,
       type: string,
       market: string,
-      limit: number
+      limit: number,
+      offset: number
     ) => {
       const response = await fetch(
         `${BASE_URI}/search?q=${encodeURI(song)}&type=${encodeURI(
           type
-        )}&market=${encodeURI(market)}&limit=${limit}`,
+        )}&market=${encodeURI(market)}&limit=${limit}&offset=${offset}`,
         {
           method: "GET",
           headers: {
@@ -55,14 +57,43 @@ export default async function handler(req, res) {
       return response.json();
     };
 
+    const songScore = (song1: string, song2: string) => {
+      const result = fuzzysort.single(song1, song2);
+      if (result) {
+        console.log("result:", result);
+        const score = result!.score;
+        return score;
+      } else {
+        return -1000;
+      }
+    };
+
     const addSongs = async (_playlist_id: string) => {
       while (songs.length !== 0) {
-        const song = songs.shift();
-        const song_response = await searchSong(song, "track", "US", 1);
-        const song_items = JSON.parse(
-          JSON.stringify(song_response.tracks.items)
-        );
-        const song_item = song_items[0];
+        let song = songs.shift();
+        let highest_score = -1000;
+        let index = 0;
+        let song_item;
+
+        while (index < 4 && highest_score < 0) {
+          let song_response = await searchSong(song, "track", "US", 50, index);
+          console.log(song_response);
+          let song_items = JSON.parse(
+            JSON.stringify(song_response.tracks.items)
+          );
+
+          for (let i = 0; i < song_items.length; i++) {
+            let score = songScore(song, song_items[i].name);
+            if (highest_score < score) {
+              highest_score = score;
+              song_item = song_items[i];
+            }
+            if (highest_score == 0) {
+              break;
+            }
+          }
+          index++;
+        }
         if (song_item) {
           const song_uri = song_item.uri;
           addSong(_playlist_id, [song_uri]);
